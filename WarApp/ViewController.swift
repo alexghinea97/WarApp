0257b4f3eb64fd8ec3e90a3194cea06fd7f77766
alexghinea97 <alexghinea97@gmail.com>
2018-06-16 00:50:11 +0300

    add actions

--- WarApp/ViewController.swift
+++ WarApp/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leftImageView: UIImageView!
+    
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func dealTapped(_ sender: Any) {
+        
+    }
+    
 }
 
